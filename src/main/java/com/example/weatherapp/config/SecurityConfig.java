92a22738971a0ba32a431174d018c2642d36be8d
Gunangi <bhagatgunangi@gmail.com>
2025-07-23 13:45:21 +0530

    Add files via upload

--- src/main/java/com/example/weatherapp/config/SecurityConfig.java
+++ src/main/java/com/example/weatherapp/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.weatherapp.config;
 
-import com.example.weatherapp.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
