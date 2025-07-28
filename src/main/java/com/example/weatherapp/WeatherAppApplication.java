20d57b166713037532fe1a361fefc54a9f559f0f
Gunangi <bhagatgunangi@gmail.com>
2025-07-28 21:30:33 +0530

    Add files via upload

--- src/main/java/com/example/weatherapp/WeatherAppApplication.java
+++ src/main/java/com/example/weatherapp/WeatherAppApplication.java
@@ -2,12 +2,13 @@ package com.example.weatherapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.example.weatherapp.repository")
 public class WeatherAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WeatherAppApplication.class, args);
     }
-
 }
